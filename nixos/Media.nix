b0e758200a0388294f9673b4d3d3d1276e7838f3
Cale <cssjacobi@gmail.com>
2026-03-29 12:06:02 +1000

    first(10th) pass for nixflix setup

--- nixos/Media.nix
+++ nixos/Media.nix
@@ -1,13 +1,25 @@
 { config, lib, pkgs, sources, ... }:
 let
   nixflix = import sources.nixflix;
+  pkgs-uns = import sources.pkgs-uns {
+    system = "x86_64-linux"; # Adjust to your system
+  };
 in {
   imports = [
     nixflix.nixosModules.default
+    ../Compose2Nix/octo-fiesta.nix
+  ];
+
+  octo-fiesta.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    beets
   ];
 
   # nix.settings.fallback = true; #temporary?
   users.users.admin.extraGroups = [ "media" ];
+
+  users.groups.media = {};
 
   sops.secrets = {
     jelly-pass = {};
@@ -18,6 +30,11 @@ in {
     anime-api = {};
     prowlarr-api = {};
     mullvad-acc = {};
+    NZBStars-api = {};
+    sabnzbd-api = {};
+    sabnzbd-nzb = {};
+    eweka-user = {};
+    eweka-pass = {};
   };
   services.syncthing = {
     enable = true;
@@ -28,9 +45,10 @@ in {
   };
 
   nixflix = {
-    enable = true;
+    enable = false;
     mediaDir = "/mnt/storage/media";
     stateDir = "/data/.state";
+    downloadsDir = "/mnt/storage/mediadata";
 
     theme = {
       enable = true;
@@ -55,7 +73,45 @@ in {
          hostConfig = {
           password = {_secret = config.sops.secrets.jelly-pass.path;};
           username = "admin";
+         };
+         indexers = [
+           {
+             name = "NZBStars";
+             apiKey = {_secret = config.sops.secrets.NZBStars-api.path;};
+           }
+           { name = "BitSearch"; }
+           { name = "Bangumi Moe"; }
+           { name = "BT.etree"; }
+           { name = "EZTV"; }
+           { name = "Knaben"; }
+           { name = "nekoBT"; }
+           { name = "SubsPlease"; }
+         ];
+      };
+    };
+
+    sabnzbd = {
+      enable = true;
+      settings = {
+        misc = {
+          api_key = {_secret = config.sops.secrets.sabnzbd-api.path;};
+          nzb_key = {_secret = config.sops.secrets.sabnzbd-nzb.path;};
         };
+
+        servers = [
+          {
+            name = "Eweka";
+            host = "sslreader.eweka.nl";
+            port = 563;
+            # Secrets use { _secret = /path; } syntax
+            username = {_secret = config.sops.secrets.eweka-user.path;};
+            password = {_secret = config.sops.secrets.eweka-pass.path;};
+            connections = 20;
+            ssl = true;
+            priority = 0;
+            retention = 3000;
+          }
+        ];
       };
     };
 
@@ -71,7 +127,7 @@ in {
     };
 
     sonarr-anime = {
-      enable = true;
+      enable = false;
       config = {
         apiKey = {_secret = config.sops.secrets.anime-api.path;};
          hostConfig = {
@@ -85,7 +141,10 @@ in {
       enable = true;
       config = {
         apiKey = {_secret = config.sops.secrets.radarr-api.path;};
-        hostConfig.password = {_secret = config.sops.secrets.jelly-pass.path;};
+        hostConfig = {
+          password = {_secret = config.sops.secrets.jelly-pass.path;};
+          username = "admin";
+        };
        };
     };
 
@@ -98,12 +157,15 @@ in {
       enable = true;
       config = {
         apiKey = {_secret = config.sops.secrets.lidarr-api.path;};
-        hostConfig.password = {_secret = config.sops.secrets.jelly-pass.path;};
+        hostConfig = {
+          password = {_secret = config.sops.secrets.jelly-pass.path;};
+          username = "admin";
+        };
        };
     };
 
     jellyseerr = {
-      enable = true;
+      enable = false;
       vpn.enable = true;
       apiKey = {_secret = config.sops.secrets.seerr-api.path;};
     };
@@ -116,6 +178,41 @@ in {
       killSwitch.enable = true;
     };
   };
+
+  networking.firewall.checkReversePath = "loose";
+
+  services = {
+    tailscale = {
+      useRoutingFeatures = "both";
+      extraSetFlags = [ "--advertise-exit-node" ];
+    };
+    deluge = {
+      enable = true;
+      web.enable = true;
+      # declarative = true;
+      group = "media";
+      #config = {
+      #  download_location = "/data/downloads/torrents/incomplete";
+      #};
+    };
+    navidrome = {
+      enable = true;
+      group = "media";
+      settings = {
+        MusicFolder = "/mnt/storage/media/music";
+        Address = "0.0.0.0";
+      };
+    };
+  };
+
+nixpkgs.overlays = [ # Fix for Navidrome being busted in version 0.59
+  (self: super: {
+    navidrome = self.callPackage (pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/cimm/nixpkgs/71aa374ad541b41e6fccd543c67b6952d2ccafca/pkgs/by-name/na/navidrome/package.nix";
+      sha256 = "16mfj85w8d7vzc9pgcgjn7a71z7jywqpdn8igk9zp0hw9dvm9rmq";
+    }) {};
+  })
+];
 
 networking.nftables = {
   enable = true;
